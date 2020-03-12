b4e359eaab84bfb777fc1ac92760bd41df8a3e83
Dallas Goldswain <dallasg@ornicogroup.co.za>
2020-03-12 11:30:17 +0200

    Added Rails V6 check to generator migration_version

--- lib/generators/counter_culture_generator.rb
+++ lib/generators/counter_culture_generator.rb
@@ -28,11 +28,8 @@ class CounterCultureGenerator < ActiveRecord::Generators::Base
   end
 
   def migration_version
-    if Rails.version.start_with? '5.'
-      "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
-    end
-    if Rails.version.start_with? '6.'
-      "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
-    end
+    return unless ['5.', '6.'].any? { |i| include? i }
+
+    "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]"
   end
 end
